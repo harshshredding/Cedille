3d610f992696c3149f6b47465595e1039f9ab100
Sakari Woods <sakariwoods@gmail.com>
2019-07-13 15:05:11 -0700

    Added click functionality (#11)
    * Initial file creation.
    
    * Added default logo icon.
    
    * Removed comment.
    
    * Fixed merge error.
    
    * Initial file creation.
    
    * Added options extension, listening event(will remove) and demo test page.
    
    * Initial file creation.
    
    * Added click functionality.

---
+++ scripts/cedille.js
@@ -0,0 +1,8 @@
+console.log("cedille started");
+chrome.browserAction.onClicked.addListener(function (tab) {
+        chrome.tabs.executeScript(tab.id, {
+            "file": "scripts/evaluate.js"
+        }, function () { //Can potentially remove this.
+            console.log("Evaluate function was successful.");
+    });
+});
