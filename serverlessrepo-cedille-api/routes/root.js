c3e1843f3c73ab49473436a1937acd8392b81b8e
max-mcd <max.mcd@outlook.com>
2019-07-14 03:15:24 +0000

    working double translation

--- serverlessrepo-cedille-api/routes/root.js
+++ serverlessrepo-cedille-api/routes/root.js
@@ -9,22 +9,46 @@ var AWS = require("aws-sdk");
  * the GET method, so we do not have to handle OPTIONS requests.
  * Any desired additional CORS headers should be included in the GET response.
  */
-exports.handler = async (event, context, callback) => {
+exports.handler = async function (event, context) {
+  const origin = cors.getOriginFromEvent(event);
+  var input_text = event.queryStringParameters.text;     
 
-    const origin = cors.getOriginFromEvent(event);
+  var params1 = {
+    SourceLanguageCode: 'fr', /* required */
+    TargetLanguageCode: 'en', /* required */
+    Text: input_text
+  };
+	
+  var request1 = new AWS.Translate().translateText(params1);
     
-    let request = new AWS.Comprehend().detectDominantLanguage({Text: 'Je suis allemand.'});
+  // create the promise object
+  var promise1 = request1.promise();
+    
+  // handle promise's fulfilled/rejected states
+  var data =  await promise1;
+    console.log(data);
+    var en_text = data.TranslatedText;
+
+  var params2 = {
+    SourceLanguageCode: 'en', /* required */
+    TargetLanguageCode: 'fr', /* required */
+    Text: en_text
+  };
+	
+  var request2 = new AWS.Translate().translateText(params2);
     
     // create the promise object
-    let promise = request.promise();
+  var promise2 = request2.promise();
     
-    let result = await promise;
+    // handle promise's fulfilled/rejected states
+  
+  data =  await promise2;
+    console.log(data);
+    var fr_text = data.TranslatedText;
     
-    console.log(result);
-    
-    return {
+  return {
             headers: cors.createOriginHeader(origin, config.allowedOrigins),
             statusCode: 200,
-            body: JSON.stringify({ text: event.queryStringParameters.text })
+            body: JSON.stringify({ text: fr_text })
         };
 };
