9c4e54a703cae1adf8fc4ffdf2d3eaae45e7ef88
Asimov4 <asimov4@gmail.com>
2019-07-14 01:54:22 +0000

    Fix the API with async and await
    M: fixes the bug that causes  before.

--- serverlessrepo-cedille-api/routes/root.js
+++ serverlessrepo-cedille-api/routes/root.js
@@ -9,28 +9,22 @@ var AWS = require("aws-sdk");
  * the GET method, so we do not have to handle OPTIONS requests.
  * Any desired additional CORS headers should be included in the GET response.
  */
-exports.handler = (event, context) => {
+exports.handler = async (event, context, callback) => {
 
     const origin = cors.getOriginFromEvent(event);
     
-    var request = new AWS.Comprehend().detectDominantLanguage({Text: 'Je suis allemand.'});
+    let request = new AWS.Comprehend().detectDominantLanguage({Text: 'Je suis allemand.'});
     
     // create the promise object
-    var promise = request.promise();
+    let promise = request.promise();
     
-    // handle promise's fulfilled/rejected states
-    promise.then(
-      function(data) {
-        console.log(data);
-        return {
+    let result = await promise;
+    
+    console.log(result);
+    
+    return {
             headers: cors.createOriginHeader(origin, config.allowedOrigins),
             statusCode: 200,
             body: JSON.stringify({ text: event.queryStringParameters.text })
         };
-      },
-      function(error) {
-        /* handle the error */
-        console.log(error)
-      }
-    );
 };
