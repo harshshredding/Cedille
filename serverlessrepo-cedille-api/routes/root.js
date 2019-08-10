ea06c4c4b6d3b717d99e0b19fcfdb615929b75ad
Asimov4 <asimov4@gmail.com>
2019-08-10 21:48:16 +0000

    Fixing bugs and updating from batch to single doc detect language

--- serverlessrepo-cedille-api/routes/root.js
+++ serverlessrepo-cedille-api/routes/root.js
@@ -3,25 +3,24 @@ const config = require("../cors-config.json");
 var AWS = require("aws-sdk");
 
 exports.handler = async function (event, context) {
-  const origin = cors.getOriginFromEvent(event);
   var input_text = event.queryStringParameters.text;     
 
   // Detect Dominant Language
-  var detectLanguageParams = {TextList: [input_text]};
-  var detectLanguageRequest = new AWS.Comprehend().batchDetectDominantLanguage(detectLanguageParams);
+  var detectLanguageParams = {Text: input_text};
+  var detectLanguageRequest = new AWS.Comprehend().detectDominantLanguage(detectLanguageParams);
   var detectLanguageResponse =  await detectLanguageRequest.promise();
-  var input_language = detectLanguageResponse.ResultList[0].Languages[0].LanguageCode;	
+  var input_language = detectLanguageResponse.Languages[0].LanguageCode;	
 	
 	// Translate to English
-  var en_text = await translate(input_text,input_language,'en').TranslatedText;
+  var en_text = await translate(input_text,input_language,'en');
 
   // Translate back to original language
-  var accented_text = await translate(en_text,'en',input_language).TranslatedText;
+  var accented_text = await translate(en_text.TranslatedText,'en',input_language);
     
   return {
             headers: {"Access-Control-Allow-Origin": "*"},
             statusCode: 200,
-            body: JSON.stringify({ text: accented_text })
+            body: JSON.stringify({ text: accented_text.TranslatedText })
         };
 };
 
