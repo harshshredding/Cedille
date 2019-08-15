1f11d050108671a04a3c938d7a9839962650792c
Harsh Verma <harshshredding@gmail.com>
2019-08-15 14:16:55 -0700

    correct accents by selecting character. Add accents to selected text.

--- chrome-extension-cedille/scripts/cedille.js
+++ chrome-extension-cedille/scripts/cedille.js
@@ -1,9 +1,15 @@
 console.log("cedille started");
-
+var count = 0;
 chrome.browserAction.onClicked.addListener(function (tab) {
-        chrome.tabs.executeScript(tab.id, {
-            "file": "scripts/evaluate.js"
-        }, function () { //Can potentially remove this.
-            console.log("Evaluate function was successful.");
+    count++;
+    // Below is the way I found to persis `count` across different plugin
+    // calls.
+    chrome.tabs.executeScript(tab.id, {code: 'var count = ' + count + ";"},
+        function() {
+            chrome.tabs.executeScript(tab.id, {
+                "file": "scripts/evaluate.js"
+            }, function () { //Can potentially remove this.
+                console.log("Evaluate function was successful.");
+            });
     });
 });
